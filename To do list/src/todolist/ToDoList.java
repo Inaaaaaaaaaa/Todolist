cbdb671dfc0aeaad6919da406973e636c200848e
Ina <inayumi7@gmail.com>
2023-11-08 14:39:08 +1300

    Creating a new frame
    - testing case for ToDoList passed
    
    -Create a new frame for to do list

---
+++ To do list/src/todolist/ToDoList.java
@@ -0,0 +1,26 @@
+package todolist;
+
+//imports
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+/**
+ *
+ * @author University
+ */
+
+    
+    /**
+     * @param args the command line arguments
+     */
+
+public class ToDoList {
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("To do list");
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,300);
+        frame.setVisible(true);
+    }
+}
---
+++ To do list/test/todolist/ToDoListIT.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package todolist;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author University
+ */
+public class ToDoListIT {
+    
+    public ToDoListIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class ToDoList.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        ToDoList.main(args);
+    }
+    
+}
